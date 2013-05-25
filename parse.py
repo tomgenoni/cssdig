8a96d659dd2770987bbe33064fa206a7416757ff
Tom Genoni <tom@atomeye.com>
2013-05-25 00:32:55 -0700

    Fixing query string.

--- parse.py
+++ parse.py
@@ -5,7 +5,7 @@ from collections import Counter
 from bs4 import BeautifulSoup
 from cStringIO import StringIO
 
-url = "http://www.twitter.com/"
+url = "http://www.huffingtonpost.com/"
 #url = "http://atomeye.com"
 
 # Domains that can't or shouldn't be included.
@@ -48,8 +48,10 @@ timestamp = datetime.datetime.fromtimestamp(ts).strftime('%m-%d-%Y at %H:%M:%S')
 # Parse the url.
 url_parsed = urlparse(url)
 
-css_urls = []
+css_urls_all = []
+css_urls_clean = []
 css_combined = ""
+css_urls_list = ""
 
 def getRemoteURL(url):
     doc = urllib2.urlopen(url)
@@ -71,32 +73,32 @@ for link in soup.find_all('link'):
         css_href_parsed = urlparse(link.get('href'))
 
         # Test for query string, include only if needed.
-        query_string = None
-        if not css_href_parsed.query == None:
+        query_string = ""
+        if not css_href_parsed.query == "":
             query_string = "?" + css_href_parsed.query
 
         # Resolve the path to the CSS files.
         full_css_path = urlunparse((css_href_parsed.scheme or url_parsed.scheme, css_href_parsed.netloc or url_parsed.netloc, os.path.join(os.path.dirname(url_parsed.path), css_href_parsed.path + query_string), None, None, None))
 
         #Create list of CSS files on the page.
-        css_urls.append(full_css_path)
+        css_urls_all.append(full_css_path)
 
 # Go through all the css paths.
-for u in css_urls:
+for u in css_urls_all:
     host = urlparse(u).hostname
     # Concatenate all CSS files into one long string if they are not blacklisted.
     if not host in domain_blacklist:
         css_combined += getRemoteURL(u)
+        css_urls_clean.append(u)
 
-# Start collecting the HTML.
-html = "<table class='stats'><tr><td><b>CSS File:</b></td><td><a href='TODO'/>TODO</a></td></tr>\n"
-html += "<tr><td><b>Created:</b></td><td>"+timestamp+"</td></tr></table>\n"
 
 # Find all instances of !important.
 important_values = re.findall("!important", css_combined)
+html = "<div class='table-wrap'>\n"
 html += "<table>\n"
 html += "<tr class='totals'>\n<td>!important</td>" + "<td>" + str(len(important_values)) + "</td>\n</tr>\n"
 html += "</table>\n"
+html += "</div>\n"
 
 # Run through all the properties.
 for p in properties:
@@ -105,6 +107,7 @@ for p in properties:
     values.sort()
     cnt = Counter(values)
 
+    html += "<div class='table-wrap'>\n"
     html += "<table>\n"
     html += "<tr class='totals'>\n<td>"+p+"</td>" + "<td>" + str(len(values)) + "</td>\n</tr>\n"
 
@@ -115,9 +118,20 @@ for p in properties:
             color_example = "<span class='color-example' style='background:"+key+"'></span>"
         html += "<tr>\n<td>" + color_example + p +": " + "%s;</td><td>%s</td>\n</tr>\n" % (key, value)
     html += "</table>\n"
+    html += "</div>\n"
+
+for u in css_urls_clean:
+    css_urls_list += "<li>" + u + "</li>"
+
+# Start collecting the HTML.
+header = "<table class='stats'>\n"
+header += "<tr><td>Target URL</td><td><a href='TODO'/>"+url+"</a></td></tr>\n"
+header += "<tr><td>CSS Files</td><td><ul>" + css_urls_list + "</ul></td></tr>\n"
+header += "<tr><td>Created</td><td>"+timestamp+"</td></tr>\n"
+header += "</table>\n"
 
 # Open build file, replace placeholders.
 tf = open(build_file, 'w')
-final_output = layout_tmpl_html.replace("{{ results }}", html)
+final_output = layout_tmpl_html.replace("{{ results }}", header + html)
 tf.write(final_output)
 tf.close
