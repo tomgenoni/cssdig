054a1e8596cec23e4c24fb0ebb591defc5e3ee5b
Tom Genoni <tom@atomeye.com>
2013-05-24 23:28:17 -0700

    Commenting.

--- parse.py
+++ parse.py
@@ -5,15 +5,45 @@ from collections import Counter
 from bs4 import BeautifulSoup
 from cStringIO import StringIO
 
-url = "http://www.huffingtonpost.com/"
+url = "http://www.twitter.com/"
 #url = "http://atomeye.com"
 
-# Domains that can't be accessed by the script.
+# Domains that can't or shouldn't be included.
 domain_blacklist = [
     'cloud.typography.com',
     'use.typekit.com',
     'fonts.googleapis.com'
 ]
+
+# Properties to be tested.
+properties = [
+    'background',
+    'border',
+    'color',
+    'font',
+    'font-size',
+    'font-family',
+    'font-weight',
+    'line-height',
+    'margin',
+    'margin-left',
+    'margin-right',
+    'padding',
+    'padding-left',
+    'padding-right',
+    'width',
+    'height',
+    'z-index'
+]
+
+build_dir = 'output'
+build_file = build_dir+'/index.html'
+layout_tmpl_html = open('template/index.tmpl').read()
+
+# Get timestamp.
+ts = time.time()
+timestamp = datetime.datetime.fromtimestamp(ts).strftime('%m-%d-%Y at %H:%M:%S')
+
 
 # Parse the url.
 url_parsed = urlparse(url)
@@ -40,6 +70,7 @@ for link in soup.find_all('link'):
         # If it's a stylesheet, get the link to the css sheet.
         css_href_parsed = urlparse(link.get('href'))
 
+        # Test for query string, include only if needed.
         query_string = None
         if not css_href_parsed.query == None:
             query_string = "?" + css_href_parsed.query
@@ -50,50 +81,25 @@ for link in soup.find_all('link'):
         #Create list of CSS files on the page.
         css_urls.append(full_css_path)
 
+# Go through all the css paths.
 for u in css_urls:
     host = urlparse(u).hostname
     # Concatenate all CSS files into one long string if they are not blacklisted.
     if not host in domain_blacklist:
         css_combined += getRemoteURL(u)
 
-props = [
-    'background',
-    'border',
-    'color',
-    'font',
-    'font-size',
-    'font-family',
-    'font-weight',
-    'line-height',
-    'margin',
-    'margin-left',
-    'margin-right',
-    'padding',
-    'padding-left',
-    'padding-right',
-    'width',
-    'height',
-    'z-index'
-]
-
-build_dir = 'output'
-build_file = build_dir+'/index.html'
-layout_tmpl_html = open('template/index.tmpl').read()
-
-ts = time.time()
-timestamp = datetime.datetime.fromtimestamp(ts).strftime('%m-%d-%Y at %H:%M:%S')
-
-# Star collecting the HTML.
+# Start collecting the HTML.
 html = "<table class='stats'><tr><td><b>CSS File:</b></td><td><a href='TODO'/>TODO</a></td></tr>\n"
 html += "<tr><td><b>Created:</b></td><td>"+timestamp+"</td></tr></table>\n"
 
-# Find all instances of !important
+# Find all instances of !important.
 important_values = re.findall("!important", css_combined)
 html += "<table>\n"
 html += "<tr class='totals'>\n<td>!important</td>" + "<td>" + str(len(important_values)) + "</td>\n</tr>\n"
 html += "</table>\n"
 
-for p in props:
+# Run through all the properties.
+for p in properties:
     regex = "(?<!-)"+p+"\s*:\s*(.*?)[;|}]"
     values = re.findall(regex, css_combined)
     values.sort()
@@ -110,6 +116,7 @@ for p in props:
         html += "<tr>\n<td>" + color_example + p +": " + "%s;</td><td>%s</td>\n</tr>\n" % (key, value)
     html += "</table>\n"
 
+# Open build file, replace placeholders.
 tf = open(build_file, 'w')
 final_output = layout_tmpl_html.replace("{{ results }}", html)
 tf.write(final_output)
