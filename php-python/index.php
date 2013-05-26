2a7a9ec484c24d453ef16502993dc94fb13abee8
Tom Genoni <tom@atomeye.com>
2013-05-26 16:44:22 -0700

    Working with URL from form.

--- php-python/index.php
+++ php-python/index.php
@@ -2,24 +2,60 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>CSS Report</title>
+    <link href='../assets/new.css' rel='stylesheet' type='text/css'>
 </head>
-<body>
 
-<?php
-    $checked = $_GET['x'];
-    $comma_separated = implode(",", $checked);
-    $tmp = exec("python build.py '". $comma_separated ."'");
-    echo $tmp;
-?>
+    <nav>
+        <h1>CSS Peek</h1>
+        <p class="desc">
+            CSS Peek groups CSS properties and values to help you refactor and standardize. That or just stare in horror.
+        </p>
+        <p>
+            <a href="#">About</a> | <a href="#">GitHub</a>
+        </p>
+        <form action="" method="get">
+            <label for="">Enter a URL</label>
+            <input type="text" id="url" name="url" value="http://atomeye.com" />
+            <div class="props">
+                <div class="col-left">
+                    <div>
+                        <input type="checkbox" name="p[]" value="background" checked="checked">
+                        <label for="">background</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="p[]" value="border">
+                        <label for="">border</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="p[]" value="color">
+                        <label for="">color</label>
+                    </div>
+                </div>
+                <div class="col-right">
+                </div>
+            </div>
+            <button class="button">Peek</button>
+        </form>
+    </nav>
 
-<form action="./" method="get">
+    <div class="content">
+    <?php
+        $url = $_GET['url'];
+        $properties = $_GET['p'];
+        $properties_separated = implode(",", $properties);
+        $cmd = 'python build.py '.$url.' '.$comma_separated;
+        $results = shell_exec($cmd);
+        echo $results;     
+    ?>
+    </div>
+<!-- <form action="./" method="get">
     <input type="checkbox" name="x[]" value="Bike" />
     <input type="checkbox" name="x[]" value="Car" />
     <input type="checkbox" name="x[]" value="Plane" />
     <input type="submit" value="Submit" />
 </form>
-
+ -->
 
 </body>
 </html>
