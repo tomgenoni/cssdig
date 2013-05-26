95d0b219a87a98b5d8bbc5ae9e45a25a92765107
Tom Genoni <tom@atomeye.com>
2013-05-25 23:30:13 -0700

    Fixing passing of form elements.

--- php-python/index.php
+++ php-python/index.php
@@ -7,12 +7,20 @@
 <body>
 
 <?php
-
     $checked = $_GET['x'];
     $comma_separated = implode(",", $checked);
     $tmp = exec("python build.py '". $comma_separated ."'");
     echo $tmp;
 ?>
+
+<form action="./" method="get">
+    <input type="checkbox" name="x[]" value="Bike" />
+    <input type="checkbox" name="x[]" value="Car" />
+    <input type="checkbox" name="x[]" value="Plane" />
+    <input type="submit" value="Submit" />
+</form>
+
+
 </body>
 </html>
 
