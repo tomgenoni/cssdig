c7d1a2182aaa1980eb5af7868e7828c79284eaee
Tom Genoni <tom@atomeye.com>
2013-06-06 12:18:17 -0700

    Try to convert declaration matches to tinycss.

--- css.py
+++ css.py
@@ -3,4 +3,9 @@ import tinycss
 parser = tinycss.make_parser()
 stylesheet = parser.parse_stylesheet("body { background: red} ")
 
-print stylesheet
+properties = []
+
+for r in stylesheet.rules:
+    for d in r.declarations:
+        for tok in d.value:
+            print d.name + ": " + tok.value + ";"
