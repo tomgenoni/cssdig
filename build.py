93216b59369154344294d168197154d087ad60b5
Tom Genoni <tom@atomeye.com>
2013-06-11 23:40:23 -0700

    Returning command line print statement if no css found.

--- build.py
+++ build.py
@@ -144,123 +144,128 @@ style_css = ''.join([s.get_text() for s in soup.find_all('style')])
 if not style_css:
     css_combined = css_combined + style_css
 
-# Clean up CSS as needed.
-css_combined = formatCSS(css_combined)
+if css_combined == "":
+    print "No CSS found at " + url
+else:
 
-# Get number of lines in combined CSS.
-css_lines = re.findall("\n", css_combined)
-css_lines = str(len(css_lines))
+    # Clean up CSS as needed.
+    css_combined = formatCSS(css_combined)
 
-print "Building report..."
+    # Get number of lines in combined CSS.
+    css_lines = re.findall("\n", css_combined)
+    css_lines = str(len(css_lines))
 
-# Find all instances of !important.
-important_values = re.findall("!important", css_combined)
-report_html_list = []
-report_html_list.append("<table class='report-entry'>\n")
-report_html_list.append("<tr class='totals'>\n")
-report_html_list.append("<td>!important</td>\n")
-report_html_list.append("<td>" + str(len(important_values)) + "</td>\n")
-report_html_list.append("</tr>\n")
-report_html_list.append("</table>\n")
+    print "Building report..."
 
-# Find all properties in the combined CSS.
-prop_regex = "[{|;]\s*([a-zA-Z0-9-]*)\s*:"
-properties = re.findall(prop_regex, css_combined)
-properties = list(set(properties))
-properties.sort()
-
-# Placholder for property checkbox HTML.
-checkbox_html_list = []
-
-# Number of properties found.
-prop_length =  str(len(properties))
-
-# Run through all the properties.
-for p in properties:
-    regex = "(?<!-)"+p+"\s*:\s*(.*?)[;|}]"
-    values = re.findall(regex, css_combined)
-    values.sort()
-    cnt = Counter(values)
-
-    prop_checked = ""
-    table_style_default ="style='display:none'"
-
-    if p in prop_on_arr:
-        prop_checked = "checked='checked' "
-        table_style_default = ""
-
-    checkbox_html_list.append("<li>\n")
-    checkbox_html_list.append("<input type='checkbox' " + prop_checked + "id='checkbox-"+p+"' name='checkbox-"+p+"'>")
-    checkbox_html_list.append("<label for='checkbox-"+p+"'>" + p + "</label>\n")
-    checkbox_html_list.append("</li>\n")
-
-    report_html_list.append("<table class='report-entry' id='table-" + p + "' "+ table_style_default +">\n")
-    report_html_list.append("<tr class='totals'>\n<td>"+p+"</td>" + "<td>" + str(len(values)) + "</td>\n</tr>\n")
-
-    for key, value in sorted(cnt.iteritems(), key=lambda (k,v): (k,v)):
-        color_example = ""
-        key = key.lstrip()
-        if p == "color" or p == "background":
-            report_html_list.append("<tr>\n<td><div class='color-example-wrap'><span class='color-example' style='background:"+key+"'></span>" + p +": " + "%s;</div></td><td>%s</td>\n</tr>\n" % (key, value))
-        elif p == "background-color":
-            report_html_list.append("<tr>\n<td><div class='color-example-wrap'><span class='color-example' style='"+p+":"+key+"'></span>" + p +": " + "%s;</div></td><td>%s</td>\n</tr>\n" % (key, value))
-        else:
-            report_html_list.append("<tr>\n<td>" + p +": " + "%s;</td><td>%s</td>\n</tr>\n" % (key, value))
+    # Find all instances of !important.
+    important_values = re.findall("!important", css_combined)
+    report_html_list = []
+    report_html_list.append("<table class='report-entry'>\n")
+    report_html_list.append("<tr class='totals'>\n")
+    report_html_list.append("<td>!important</td>\n")
+    report_html_list.append("<td>" + str(len(important_values)) + "</td>\n")
+    report_html_list.append("</tr>\n")
     report_html_list.append("</table>\n")
 
-# Join lists of html together.
-checkbox_html = ''.join(checkbox_html_list)
-report_html = ''.join(report_html_list)
+    # Find all properties in the combined CSS.
+    prop_regex = "[{|;]\s*([a-zA-Z0-9-]*)\s*:"
+    properties = re.findall(prop_regex, css_combined)
+    properties = list(set(properties))
+    properties.sort()
 
-# CSS found in <style> blocks.
-style_css = ''.join([s.get_text() for s in soup.find_all('style')])
+    # Placholder for property checkbox HTML.
+    checkbox_html_list = []
 
-# HTML for parsed CSS files.
-css_urls_list_good = ''.join(["<li><a href='"+u+"'>" + u + "</a></li>" for u in css_urls_clean])
+    # Number of properties found.
+    prop_length =  str(len(properties))
 
-# HTML for unreachable CSS files.
-if css_urls_bad:
-    css_urls_list_bad = ''.join(["<li><a href='"+b+"'>" + b + "</a></li>" for b in css_urls_bad])
+    # Run through all the properties.
+    for p in properties:
+        regex = "(?<!-)"+p+"\s*:\s*(.*?)[;|}]"
+        values = re.findall(regex, css_combined)
+        values.sort()
+        cnt = Counter(values)
 
-# Time elapsed
-time_elapsed = time.time() - start_time
-time_elapsed = str(round(time_elapsed,1))
+        prop_checked = ""
+        table_style_default ="style='display:none'"
 
-# Start collecting the HTML.
-header_html_list = []
-header_html_list.append("<div class='stats'>\n")
-header_html_list.append("<h3>URL Dug</h3>\n")
-header_html_list.append("<p><a href='"+url+"'/>"+url+"</a></p>\n")
-header_html_list.append("<h3>CSS Dug</h3>\n")
-header_html_list.append("<ul>" + css_urls_list_good + "</ul>\n")
-if css_urls_bad:
-    header_html_list.append("<h3>Skipped</h3>\n")
-    header_html_list.append("<ul class='unparsed'>" + css_urls_list_bad + "</ul>\n")
-header_html_list.append("<h3>Dug</h3>\n")
-header_html_list.append("<p>"+timestamp+" taking "+time_elapsed+" seconds</p>\n")
-header_html_list.append("</div>\n")
+        if p in prop_on_arr:
+            prop_checked = "checked='checked' "
+            table_style_default = ""
 
-# Combine header html.
-header_html = ''.join(header_html_list)
+        checkbox_html_list.append("<li>\n")
+        checkbox_html_list.append("<input type='checkbox' " + prop_checked + "id='checkbox-"+p+"' name='checkbox-"+p+"'>")
+        checkbox_html_list.append("<label for='checkbox-"+p+"'>" + p + "</label>\n")
+        checkbox_html_list.append("</li>\n")
 
-# Remove the build dir if it exists and create it.
-if os.path.isdir(build_dir):
-    shutil.rmtree(build_dir)
-os.makedirs(build_dir)
+        report_html_list.append("<table class='report-entry' id='table-" + p + "' "+ table_style_default +">\n")
+        report_html_list.append("<tr class='totals'>\n<td>"+p+"</td>" + "<td>" + str(len(values)) + "</td>\n</tr>\n")
 
-# Open layout template and replace variables.
-layout_tmpl_html = open(layout_tmpl).read()
-final_html_output = layout_tmpl_html\
-    .replace("{{ css_lines }}", css_lines)\
-    .replace("{{ prop_length }}", prop_length)\
-    .replace("{{ checkbox_html }}", checkbox_html)\
-    .replace("{{ css_combined }}", css_combined)\
-    .replace("{{ report_html }}", header_html + report_html);
+        for key, value in sorted(cnt.iteritems(), key=lambda (k,v): (k,v)):
+            color_example = ""
+            key = key.lstrip()
+            if p == "color" or p == "background":
+                report_html_list.append("<tr>\n<td><div class='color-example-wrap'><span class='color-example' style='background:"+key+"'></span>" + p +": " + "%s;</div></td><td>%s</td>\n</tr>\n" % (key, value))
+            elif p == "background-color":
+                report_html_list.append("<tr>\n<td><div class='color-example-wrap'><span class='color-example' style='"+p+":"+key+"'></span>" + p +": " + "%s;</div></td><td>%s</td>\n</tr>\n" % (key, value))
+            else:
+                report_html_list.append("<tr>\n<td>" + p +": " + "%s;</td><td>%s</td>\n</tr>\n" % (key, value))
+        report_html_list.append("</table>\n")
 
-tf = open(build_file, 'a')
-tf.write(final_html_output)
-tf.close()
+    # Join lists of html together.
+    checkbox_html = ''.join(checkbox_html_list)
+    report_html = ''.join(report_html_list)
 
-shutil.copytree('assets/', build_dir+"/assets/")
+    # CSS found in <style> blocks.
+    style_css = ''.join([s.get_text() for s in soup.find_all('style')])
 
-print "Report complete."
+    # HTML for parsed CSS files.
+    css_urls_list_good = ''.join(["<li><a href='"+u+"'>" + u + "</a></li>" for u in css_urls_clean])
+
+    # HTML for unreachable CSS files.
+    if css_urls_bad:
+        css_urls_list_bad = ''.join(["<li><a href='"+b+"'>" + b + "</a></li>" for b in css_urls_bad])
+
+    # Time elapsed
+    time_elapsed = time.time() - start_time
+    time_elapsed = str(round(time_elapsed,1))
+
+    # Start collecting the HTML.
+    header_html_list = []
+    header_html_list.append("<div class='stats'>\n")
+    header_html_list.append("<h3>URL Dug</h3>\n")
+    header_html_list.append("<p><a href='"+url+"'/>"+url+"</a></p>\n")
+    header_html_list.append("<h3>CSS Dug</h3>\n")
+    header_html_list.append("<ul>" + css_urls_list_good + "</ul>\n")
+    if css_urls_bad:
+        header_html_list.append("<h3>Skipped</h3>\n")
+        header_html_list.append("<ul class='unparsed'>" + css_urls_list_bad + "</ul>\n")
+    header_html_list.append("<h3>Dug</h3>\n")
+    header_html_list.append("<p>"+timestamp+" taking "+time_elapsed+" seconds</p>\n")
+    header_html_list.append("</div>\n")
+
+    # Combine header html.
+    header_html = ''.join(header_html_list)
+
+    # Remove the build dir if it exists and create it.
+    if os.path.isdir(build_dir):
+        shutil.rmtree(build_dir)
+    os.makedirs(build_dir)
+
+    # Open layout template and replace variables.
+    layout_tmpl_html = open(layout_tmpl).read()
+    final_html_output = layout_tmpl_html\
+        .replace("{{ url }}", url)\
+        .replace("{{ css_lines }}", css_lines)\
+        .replace("{{ prop_length }}", prop_length)\
+        .replace("{{ checkbox_html }}", checkbox_html)\
+        .replace("{{ css_combined }}", css_combined)\
+        .replace("{{ report_html }}", header_html + report_html);
+
+    tf = open(build_file, 'a')
+    tf.write(final_html_output)
+    tf.close()
+
+    shutil.copytree('assets/', build_dir+"/assets/")
+
+    print "Report complete."
